94011572e3ed0946551333a8a7b4c96b4336548c
fongyk <zsq3625@outlook.com>
2018-05-17 23:33:48 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -85,11 +85,11 @@ html_theme = 'sphinx_rtd_theme'
 
 html_show_sourcelink = True
 
-html_context = {
-"display_github": False, # Add 'Edit on Github' link instead of 'View page source'
-"last_updated": True,
-"commit": False,
-}
+# html_context = {
+# "display_github": True, # Add 'Edit on Github' link instead of 'View page source'
+# "last_updated": True,
+# "commit": False,
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

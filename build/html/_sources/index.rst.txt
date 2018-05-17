1af582656d49dd89b6a0ad3dee51b418923724c7
fongyk <zsq3625@outlook.com>
2018-05-17 23:07:43 +0800

    update

--- build/html/_sources/index.rst.txt
+++ build/html/_sources/index.rst.txt
@@ -3,12 +3,12 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-文档:
+文档
 ========
 
 .. toctree::
-   :maxdepth: 2
-   :glob:
+   :maxdepth: 3
+   :caption: 目录
 
    cpp/index
    python/index
--- source/conf.py
+++ source/conf.py
@@ -83,6 +83,11 @@ html_theme = 'sphinx_rtd_theme'
 #
 # html_theme_options = {}
 
+html_show_sourcelink = True
+html_context = {
+"display_github": False,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

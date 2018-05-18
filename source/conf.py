46aa9e7c93d3908e1e17b559dd8da6a921113d5e
fongyk <zsq3625@outlook.com>
2018-05-18 14:38:38 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -60,7 +60,9 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'python'
+
+highlight_language = "none"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.

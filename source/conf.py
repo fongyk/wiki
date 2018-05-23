e4b110a4a12733c0db69893f72cb83a1bfbea564
fongyk <zsq3625@outlook.com>
2018-05-24 00:21:34 +0800

    update conf

--- source/conf.py
+++ source/conf.py
@@ -18,6 +18,14 @@
 
 import sphinx_rtd_theme
 
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # import os
 # on_rtd = os.environ.get('READTHEDOCS') == 'False'
 # if on_rtd:
